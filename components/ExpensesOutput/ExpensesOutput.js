188ea98b3e935c8a46dbe84e4d92071291f031a8
Chuks Mgbojikwe <chuxmgbojikwe@gmail.com>
2024-05-10 03:45:19 +0700

    Outputting a List of Expenses

---
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -0,0 +1,47 @@
+import { View } from "react-native";
+import ExpenesSummary from "./ExpensesSummary";
+import ExpenesList from "./ExpensesList";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    description: "Bought sum fruits",
+    amount: 69.99,
+    date: new Date("2023-12-14"),
+  },
+  {
+    id: "e2",
+    description: "Bought a pair of shoe",
+    amount: 80.0,
+    date: new Date("2023-12-14"),
+  },
+  {
+    id: "e3",
+    description: "Bought a pair of shoe",
+    amount: 55.99,
+    date: new Date("2023-07-10"),
+  },
+  {
+    id: "e4",
+    description: "Stationaries",
+    amount: 18.99,
+    date: new Date("2023-03-14"),
+  },
+  {
+    id: "e5",
+    description: "Some More Stationaries",
+    amount: 14.75,
+    date: new Date("2023-02-19"),
+  },
+];
+
+function ExpensesOutput({ expenses, expensesPeriod }) {
+  return (
+    <View>
+      <ExpenesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
+      <ExpenesList expenses={DUMMY_EXPENSES} />
+    </View>
+  );
+}
+
+export default ExpensesOutput;
