2e9cd52934f364b565f6760b32180f56397069ed
Chuks Mgbojikwe <chuxmgbojikwe@gmail.com>
2024-05-09 07:22:15 +0700

    Adding Global Colors & Editing Navigation Configuration

---
+++ constants/styles.js
@@ -0,0 +1,16 @@
+export const GlobalStyles = {
+  colors: {
+    primary100: "#dbe4e0",
+    primary200: "#BFC9C4",
+    primary400: "#94A89E",
+    primary500: "#5E8170",
+    primary700: "#2C5541",
+    primary800: "#00130A",
+    accent500: "#03DAC5",
+    error100: "#FFDAD6",
+    error500: "#BA1A1A",
+    gray200: "#FDF7FF",
+    gray700: "#1D1B20",
+    white: "#FFFFFF",
+  },
+};
