2ec4f5f95e211f936980321871bde9a85259c5a2
ChuxMg <chuxmgbojikwe@gmail.com>
2024-06-21 10:16:05 +0700

    Managing Form State & Submission

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -2,77 +2,98 @@ import { StyleSheet, Text, View } from "react-native";
 import Input from "./Input";
 import { GlobalStyles } from "../../constants/styles";
 import { useState } from "react";
+import Button from "../UI/Button";
 
-function ExpenseForm() {
-    const [inputValues, setInputValues] = useState({
-        amount: "",
-        date: "",
-        description: ""
+function ExpenseForm({ submitButtonLabel, onCancel, onSubmit }) {
+  const [inputValues, setInputValues] = useState({
+    amount: "",
+    date: "",
+    description: "",
+  });
+
+  function inputChangedHandler(inputIdentifier, enteredValue) {
+    setInputValues((currentInputValues) => {
+      return {
+        ...currentInputValues,
+        [inputIdentifier]: enteredValue,
+      };
     });
+  }
 
-    function inputChangedHandler( inputIdentifier, enteredValue) {
-        setInputValues((currentInputValues) => {
-            return {
-                ...currentInputValues,
-                [inputIdentifier]: enteredValue
-            }
-        });
-    };
+  function submitHandler() {}
 
-    return (
-      <View style={styles.form}>
-        <Text style={styles.title}>Your Expense</Text>
-        <View style={styles.inputsRow}>
-          <Input
-            style={styles.rowInput}
-            label="Amount"
-            textInputConfig={{
-              keyboardType: "decimal-pad",
-              onChangeText: inputChangedHandler.bind(this, "amount"),
-              value: inputValues.amount,
-            }}
-          />
-          <Input
-            style={styles.rowInput}
-            label="Date"
-            textInputConfig={{
-              placeholder: "YYYY-MM-DD",
-              maxLength: 10,
-              onChangeText: inputChangedHandler.bind(this, "date"),
-              value: inputValues.date,
-            }}
-          />
-        </View>
+  return (
+    <View style={styles.form}>
+      <Text style={styles.title}>Your Expense</Text>
+      <View style={styles.inputsRow}>
         <Input
-          label="Description"
+          style={styles.rowInput}
+          label="Amount"
           textInputConfig={{
-            multiline: true,
-            onChangeText: inputChangedHandler.bind(this, "description"),
-            value: inputValues.description,
+            keyboardType: "decimal-pad",
+            onChangeText: inputChangedHandler.bind(this, "amount"),
+            value: inputValues.amount,
+          }}
+        />
+        <Input
+          style={styles.rowInput}
+          label="Date"
+          textInputConfig={{
+            placeholder: "YYYY-MM-DD",
+            maxLength: 10,
+            onChangeText: inputChangedHandler.bind(this, "date"),
+            value: inputValues.date,
           }}
         />
       </View>
-    );
+      <Input
+        label="Description"
+        textInputConfig={{
+          multiline: true,
+          onChangeText: inputChangedHandler.bind(this, "description"),
+          value: inputValues.description,
+        }}
+      />
+      <View style={styles.buttons}>
+        <Button style={styles.button} mode="flat" onPress={onCancel}>
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={submitHandler}>
+          {submitButtonLabel}
+        </Button>
+      </View>
+    </View>
+  );
 }
 
 export default ExpenseForm;
 
 const styles = StyleSheet.create({
-    form: {
-        marginTop: 40
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: "bold",
-        color: GlobalStyles.colors.primary800,
-        marginVertical: 24,
-        textAlign: "center"
-    },
-    inputsRow: {
-        flexDirection: "row",
-        justifyContent: "space-between"
-    },
-    rowInput: {
-        flex: 1
-    }
-})
+  form: {
+    marginTop: 40,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: GlobalStyles.colors.primary800,
+    marginVertical: 24,
+    textAlign: "center",
+  },
+  inputsRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  rowInput: {
+    flex: 1,
+  },
+
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+});
